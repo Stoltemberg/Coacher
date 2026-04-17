82aa1dda81714081c7690d9fe94d3521c89ccd8c
Gabriel Stoltemberg <tutors1997@outlook.com>
2026-04-17 17:11:06 -0300

    asd

--- site/next.config.ts
+++ site/next.config.ts
@@ -1,22 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/coacher_v1_setup.exe",
-        headers: [
-          {
-            key: "Content-Type",
-            value: "application/vnd.microsoft.portable-executable",
-          },
-          {
-            key: "Content-Disposition",
-            value: 'attachment; filename="coacher_v1_setup.exe"',
-          },
-        ],
-      },
-    ];
+  output: "export",
+  images: {
+    unoptimized: true,
   },
 };
 
