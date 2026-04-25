4a8f579d3fa7b6cca808a15cdf636de6f237c3ef
Gabriel Stoltemberg <tutors1997@outlook.com>
2026-04-24 21:52:28 -0300

    dsad

--- site/src/app/page.tsx
+++ site/src/app/page.tsx
@@ -66,7 +66,7 @@ export default function Home() {
             transition={{ duration: 1.05, ease: [0.16, 1, 0.3, 1] }}
           >
             <Image
-              src="/gameplay_overlay.png"
+              src="/gameplay_overlay.webp"
               alt="Demonstração do overlay de gameplay"
               fill
               sizes="(max-width: 1280px) 100vw, 1200px"
